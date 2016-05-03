f51db3542770121e90e3b2a2973a39f4c10b0aaf
Vishwajeet Narwal <vnarwal95@gmail.com>
2016-05-04 03:01:26 +0530

    docstrings

--- scrapping_methods.py
+++ scrapping_methods.py
@@ -1,17 +1,17 @@
-# Methods for scrapping certain information from news.google.com
+"""Methods for scrapping certain information from news.google.com"""
 
 import urllib2
 from bs4 import BeautifulSoup
 
-#Method to scrap RSS Link
 def get_rss_link(url):
+	"""Method to scrap RSS Link"""
         html = urllib2.urlopen(url).read()
         soup = BeautifulSoup(html)
         rss_link = soup.find('img','feed-icon').parent['href']
         return str(rss_link)
 
-#Method to scrap RTC Links
 def get_rtc_links(url):
+	"""Method to scrap RTC Links"""
 	html = urllib2.urlopen(url).read()
 	soup = BeautifulSoup(html)
 	links=[]
@@ -20,8 +20,8 @@ def get_rtc_links(url):
 		links.append('http://news.google.com'+str(link['href']))
 	return links
 
-#Method to scrap SEE ALL ARTICLES Link
 def get_see_all_link(url):
+	"""Method to scrap SEE ALL ARTICLES Link"""
         html = urllib2.urlopen(url).read()
         soup = BeautifulSoup(html)
         link = soup.find('a','more-coverage-text')['href']
