2e85ec7f7f0b14067e53507da7612e0211145797
Vishwajeet Narwal <vnarwal95@gmail.com>
2016-05-08 02:12:42 +0530

    method to extract ncl of a story

---
+++ regex_ops.py
@@ -0,0 +1,10 @@
+""" Methods for Regex Operations """
+import re
+
+def get_ncl(link):
+""" Method to extract ncl value of a story"""
+	ncl_string = re.findall("ncl=.*?&",link)
+	if ncl_string:
+		return ncl_string[0][4:-1]
+	else:
+		return ''
